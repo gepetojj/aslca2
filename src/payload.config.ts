955479bd125bd352b298355cc380b3cfa554ffd7
João Pedro <nmjoaopedro22@gmail.com>
2025-04-24 13:51:14 -0300

    fix: atualiza configuração

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,13 +1,13 @@
-// storage-adapter-import-placeholder
 import path from "path";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import { fileURLToPath } from "url";
 
-import { mongooseAdapter } from "@payloadcms/db-mongodb";
-// database-adapter-import
+import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
+import { pt } from "@payloadcms/translations/languages/pt";
 
 import { Media } from "./collections/Media";
 import { Users } from "./collections/Users";
@@ -17,25 +17,44 @@ const dirname = path.dirname(filename);
 
 export default buildConfig({
 	admin: {
-		user: Users.slug,
+		dateFormat: "dd/MM/yyyy 'às' HH:mm",
 		importMap: {
 			baseDir: path.resolve(dirname),
 		},
+		meta: {
+			title: "Administração - ASLCA",
+		},
+		timezones: {
+			defaultTimezone: "America/Sao_Paulo",
+		},
+		user: Users.slug,
 	},
 	collections: [Users, Media],
+	db: vercelPostgresAdapter(),
 	editor: lexicalEditor(),
+	i18n: {
+		supportedLanguages: { pt },
+	},
+	plugins: [
+		payloadCloudPlugin(),
+		vercelBlobStorage({
+			collections: {
+				media: true,
+			},
+			enabled: process.env.NODE_ENV === "production",
+			token: process.env.BLOB_READ_WRITE_TOKEN,
+		}),
+	],
 	secret: process.env.PAYLOAD_SECRET || "",
+	sharp,
 	typescript: {
 		outputFile: path.resolve(dirname, "payload-types.ts"),
 	},
-	// database-adapter-config-start
-	db: mongooseAdapter({
-		url: process.env.DATABASE_URI || "",
-	}),
-	// database-adapter-config-end
-	sharp,
-	plugins: [
-		payloadCloudPlugin(),
-		// storage-adapter-placeholder
-	],
+	upload: {
+		limits: {
+			fileSize: 10 * 1024 * 1024, // 10MB
+		},
+		preserveExtension: true,
+		safeFileNames: true,
+	},
 });
