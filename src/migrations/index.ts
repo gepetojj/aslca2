575645a0dbda25344855c1b5f777eb18aae67f4f
João Pedro <nmjoaopedro22@gmail.com>
2025-05-31 00:05:07 -0300

    feat: adiciona migratiosn

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
-import * as migration_20250521_182002_init from "./20250521_182002_init";
+import * as migration_20250521_182002_init from './20250521_182002_init';
+import * as migration_20250531_030350 from './20250531_030350';
 
 export const migrations = [
-	{
-		up: migration_20250521_182002_init.up,
-		down: migration_20250521_182002_init.down,
-		name: "20250521_182002_init",
-	},
+  {
+    up: migration_20250521_182002_init.up,
+    down: migration_20250521_182002_init.down,
+    name: '20250521_182002_init',
+  },
+  {
+    up: migration_20250531_030350.up,
+    down: migration_20250531_030350.down,
+    name: '20250531_030350'
+  },
 ];
